19afd562b20f6a444eeff11a34679bc7255d48c0
rpaskalev <rpaskalev@lomixtech.com>
2021-01-23 14:59:09 -0500

    fixed key

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -6,8 +6,8 @@ echo $BUILD_TAG >> /tmp/.auth
 
 echo $PASS >> /tmp/.auth
 
-scp -i ~/.ssh/production /tmp/.auth ec2-user@10.0.1.237:/tmp/.auth
+scp -i /opt/prod /tmp/.auth ec2-user@10.0.1.237:/tmp/.auth
 
-scp -i ~/.ssh/production ./jenkins/deploy/publish ec2-user@10.0.1.237:/tmp/publish
+scp -i /opt/prod ./jenkins/deploy/publish ec2-user@10.0.1.237:/tmp/publish
 
-ssh -i ~/.ssh/production ec2-user@10.0.1.237 "/tmp/publish"
+ssh -i /opt/prod ec2-user@10.0.1.237 "/tmp/publish"
